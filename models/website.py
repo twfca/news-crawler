862624d68a779a422733c050828d84af9fa71620
dakaizou <dakaizou@gmail.com>
2019-08-20 01:29:16 +0800

    Add url to website

--- models/website.py
+++ models/website.py
@@ -8,5 +8,6 @@ class Website(db.Entity):
     _table_ = 'websites'
     id = orm.PrimaryKey(int, auto=True)
     name = orm.Required(str, unique=True)
+    url = orm.Required(str, unique=True)
     authors = orm.Set('Author')
     articles = orm.Set('Article')
