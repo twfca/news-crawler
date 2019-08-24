5cd2231756303aa624ee66bf5b9023c93e28514a
dakaizou <dakaizou@gmail.com>
2019-08-24 22:55:49 +0800

    fix Author unique name

--- models/author.py
+++ models/author.py
@@ -8,7 +8,7 @@ db = get_database()
 class Author(db.Entity):
     _table_ = 'authors'
     id = orm.PrimaryKey(int, auto=True)
-    name = orm.Optional(str, unique=True)
+    name = orm.Optional(str)
     website = orm.Optional(Website)
     articles = orm.Set('Article')
     orm.composite_key(name, website)
