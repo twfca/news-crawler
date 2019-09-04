eab6053619b9cd87b2d6e5c1880669f221a647c1
dakaizou <dakaizou@gmail.com>
2019-09-04 11:14:47 +0800

    add get_pending_articles

--- app/controllers/article_controller.py
+++ app/controllers/article_controller.py
@@ -26,3 +26,9 @@ class ArticleController(Controller):
         articles = orm.select(a for a in Article if a.url == url).limit(1)
         if len(articles) == 1:
             return articles[0]
+
+    @orm.db_session
+    def get_pending_articles(self):
+        return orm.select(
+            a for a in Article if a.title is None or len(a.title) == 0
+        ).order_by(lambda a: a.website)
